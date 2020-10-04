96b2261c0b20748975efc44a3d6f8d5151490f03
GuilhermeViana-22 <66335015+GuilhermeViana-22@users.noreply.github.com>
2020-10-04 11:03:27 -0300

    será necessario criar um arquivo crud para cada tela

--- sistema/crud_cliente.php
+++ sistema/crud_cliente.php
@@ -8,6 +8,7 @@ if(isset($_REQUEST["ativo"])) {
 }
 if(isset($_REQUEST["id_cliente"])) {
     if(isset($_REQUEST["excluir"])) {
+          
         $consulta = "DELETE FROM cliente WHERE id_cliente = " . $_REQUEST["id_cliente"];
         # a variavel operacao está atrelada as mensagem de sucesso que aparecem na cor verde após realizar as funões do crud
         # tanto as mensagem de sucesso quando as de erro estão entrelaçadas a está variavel
@@ -27,10 +28,38 @@ if(isset($_REQUEST["id_cliente"])) {
 
 $query = $conexao->query($consulta);
 
+#verifica se houve algum erro  do crud e concatena com a variavel local operação
 if(!$query) {
     header("Location: cad_cliente.php?erro=" . $operacao);
 } else {
     header("Location: cad_cliente.php?sucesso=" . $operacao);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
